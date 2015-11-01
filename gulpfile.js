602e31f1a10e7deba7ad453d2a98a5a485c7641a
timcmiller <timcmiller@live.com>
2015-10-31 20:10:41 -0700

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var appFiles = ['lib/*.js'];
+var testFiles = ['test/*.js'];
+
+gulp.task('jshint:test', function() {
+  return gulp.src(testFiles)
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('jshint:transform', function() {
+  return gulp.src(appFiles)
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('mocha', function() {
+  return gulp.src('./test/**', {read: false})
+    .pipe(mocha({reaporter: 'nyan'}));
+});
+
+
+gulp.task('jshint', ['jshint:test', 'jshint:transform']);
+gulp.task('default', ['jshint', 'mocha']);
