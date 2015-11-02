e1b98a039a50623c1eba9a55011675fa71e7518a
timcmiller <timcmiller@live.com>
2015-11-02 10:56:35 -0800

    added tests and modulized

--- test/testbitmap.js
+++ test/testbitmap.js
@@ -1,5 +1,8 @@
 
 var expect = require('chai').expect;
+var fs = require('fs');
+var getData = require(__dirname + '/../lib/fileHandler.js');
+var img = fs.readFileSync(__dirname + '/../img/bitmap1.bmp');
 var shuffle = require(__dirname + '/../lib/transform').shuffle;
 var getDarker = require(__dirname + '/../lib/transform').getDarker;
 var getBrighter = require(__dirname + '/../lib/transform').getBrighter;
@@ -7,17 +10,15 @@ var order = require(__dirname + '/../lib/transform').order;
 var randomize = require(__dirname + '/../lib/transform').randomize;
 var inversion = require(__dirname + '/../lib/transform').inversion;
 
-testBuffer = new Buffer([1, 2, 3, 4]);
+var testBuffer = new Buffer([1, 2, 3, 4]);
 
 describe('the suffle function', function() {
-  it('should suffle an array', function() {
-    expect(shuffle([1, 2, 3, 4,])).to.not.eql([1, 2, 3, 4,]);
+  it('should suffle a buffer', function() {
+    expect(shuffle(testBuffer)).to.not.eql(new Buffer([1, 2, 3, 4]));
   });
-});
 
-describe('the order function', function() {
-  it('should order the array in numerical order', function() {
-    expect(order([3, 2, 1])).to.eql([1, 2, 3]);
+  after(function() {
+    testBuffer = new Buffer([1, 2, 3, 4]);
   });
 });
 
@@ -28,8 +29,26 @@ describe('the inversion function', function() {
   });
 });
 
-describe('the randomize function', function() {
-  it('should input random number for each input', function() {
-    expect(randomize(testBuffer)).to.not.eql(new Buffer([1, 2, 3, 4]));
+describe('the get data function', function() {
+
+  before(function() {
+    var bitmap = getData(img);
   });
+
+  it('should return the bitmap type for me', function() {
+    expect(bitmap.fileType).to.eql('BM');
+  });
+
+  it('should return header length for me', function() {
+    expect(bitmap.header).to.eql(54);
+  });
+
+  it('should return where the pixel data starts', function() {
+    expect(bitmap.pixilDataStart).to.eql(1078);
+  });
+
+  it('should return the length of the color palette', function() {
+    expect(bitmap.colorPaletteStart).to.eql(1024);
+  });
+
 });
